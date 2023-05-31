da2f1b5c37eb8144bb91311de1f445e32a9a3584
Francisco Ricaurte <guardianjavier@hotmail.com>
2023-05-30 20:25:48 -0500

    Manipulacion arreglos relaciones MaM

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -61,8 +61,24 @@ export class ProductsController {
     return this.productsService.update(id, payload);
   }
 
+  @Put(':id/category/:categoryId')
+  addCategoryToProduct(
+    @Param('id', ParseIntPipe) idProduct: number,
+    @Param('categoryId', ParseIntPipe) idCategory: number,
+  ) {
+    return this.productsService.addCategoryToProduct(idProduct, idCategory);
+  }
+
   @Delete(':id')
   delete(@Param('id') id: number) {
     return this.productsService.remove(id);
   }
+
+  @Delete(':id/category/:categoryId')
+deleteCategory(
+  @Param('id', ParseIntPipe) id: number,
+  @Param('categoryId', ParseIntPipe) categoryId: number
+){
+  return this.productsService.removeCategoryByProduct(id, categoryId);
+}
 }
